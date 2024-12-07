4992addfddadc040003cbb6bf2c9aed35bfd0647
a22i312io <a22i312io@ous.jp>
2024-12-07 18:44:51 +0900

    feat：設定画面追加

--- Assets/Okada/Scripts/menuScript/ButtonController.cs
+++ Assets/Okada/Scripts/menuScript/ButtonController.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject _firstmenu;
+    [SerializeField] private GameObject _config;
+    [SerializeField] private GameObject _basicsetting;
+    [SerializeField] private GameObject _keysetting;
+    public void ConfigChange(int i)
+    {
+        if(i == 0) {
+            _basicsetting.SetActive(true);
+            _keysetting.SetActive(false);
+        }
+        else if(i == 1) 
+        {
+            _basicsetting.SetActive(false);
+            _keysetting.SetActive(true);
+        }
+        else if (i == 2)
+        {
+            _config.SetActive(false);
+            _firstmenu.SetActive(true);
+        }
+
+    }
+
+
+
 }
--- Assets/Okada/Scripts/menuScript/UIController.cs
+++ Assets/Okada/Scripts/menuScript/UIController.cs
@@ -36,24 +36,5 @@ public class UIController : MonoBehaviour
         anim.SetBool("_isBig", false);
     }
 
-    //IEnumerator Blink()
-    //{
-    //    while (true)
-    //    {
-    //        for (int i = 0; i < 100; i++)
-    //        {
-    //            mesh.material.color = mesh.material.color - new Color32(0, 0, 0, 1);
-    //        }
-
-    //        yield return new WaitForSeconds(0.2f);
-
-    //        for (int k = 0; k < 100; k++)
-    //        {
-    //            mesh.material.color = mesh.material.color + new Color32(0, 0, 0, 1);
-    //        }
-
-    //        yield return new WaitForSeconds(0.2f);
-
-    //    }
-    //}
+    
 }
