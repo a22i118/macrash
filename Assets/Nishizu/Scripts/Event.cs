8fa90ce5290961a1448e50b9180699098e79604d
a22i118 <a22i118bw@ous.jp>
2024-12-14 20:11:14 +0900

    update：イベントの確率調整、枕の召喚イベント追加

--- Assets/Nishizu/Scripts/Event.cs
+++ Assets/Nishizu/Scripts/Event.cs
@@ -5,7 +5,6 @@ using Player;
 
 public class Event : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _players;
     [SerializeField] private List<GameObject> _hutons;
     [SerializeField] private GameObject _door;
     [SerializeField] private GameObject _teacher;
@@ -16,9 +15,11 @@ public class Event : MonoBehaviour
     private bool _isGameStart = false;
     private bool _one = false;
     private int _lastEvent = -1;
+    private int _lastEventColorChangeMakura = -1;
     private TeacherShadowController _teacherEvent;
     private MeteorEvent _meteorEvent;
     private TatamiEvent _tatamiEvent;
+    private List<GameObject> _players;
     private List<GameObject> _makuras = new List<GameObject>();
     private List<MakuraController> _makuraControllers = new List<MakuraController>();
     private List<PlayerController> _playerControllers = new List<PlayerController>();
@@ -131,10 +132,10 @@ public class Event : MonoBehaviour
         {
             _one = true;
 
-            int randomNumber = Random.Range(0, 8);
+            int randomNumber = Random.Range(0, 9);
             while (randomNumber == _lastEvent)
             {
-                randomNumber = Random.Range(0, 8);
+                randomNumber = Random.Range(0, 9);
             }
             switch (randomNumber)
             {
@@ -148,53 +149,83 @@ public class Event : MonoBehaviour
                     _meteorEvent.Init();
                     break;
                 case 3:
-                    foreach (var makura in _makuraControllers)
-                    {
-                        if (!makura.IsThrow)
-                        {
-                            makura.CurrentColorType = ColorChanger.ColorType.Red;
-                        }
-                    }
+                    RandomColorChangeMakura();
                     break;
                 case 4:
-                    foreach (var makura in _makuraControllers)
-                    {
-                        if (!makura.IsThrow)
-                        {
-                            makura.CurrentColorType = ColorChanger.ColorType.Green;
-                        }
-                    }
-                    break;
-                case 5:
-                    foreach (var makura in _makuraControllers)
-                    {
-                        if (!makura.IsThrow)
-                        {
-                            makura.CurrentColorType = ColorChanger.ColorType.Blue;
-                        }
-                    }
-                    break;
-                case 6:
-                    foreach (var makura in _makuraControllers)
-                    {
-                        if (!makura.IsThrow)
-                        {
-                            makura.CurrentColorType = ColorChanger.ColorType.Black;
-                        }
-                    }
-                    break;
-                case 7:
-                    foreach (var makura in _makuraControllers)
-                    {
-                        if (!makura.IsThrow)
-                        {
-                            makura.CurrentColorType = ColorChanger.ColorType.Nomal;
-                        }
-                    }
+                    Instantiate(_makuraPrefub, RandomPosition(), Quaternion.identity);
                     break;
             }
             _one = false;
             _lastEvent = randomNumber;
         }
+    }
+    private void RandomColorChangeMakura()
+    {
+        int randomNumberMakura = Random.Range(0, 5);
+        while (randomNumberMakura == _lastEventColorChangeMakura)
+        {
+            randomNumberMakura = Random.Range(0, 5);
+        }
+        switch (randomNumberMakura)
+        {
+            case 0:
+                foreach (var makura in _makuraControllers)
+                {
+                    if (!makura.IsThrow)
+                    {
+                        makura.CurrentColorType = ColorChanger.ColorType.Red;
+                    }
+                }
+                break;
+            case 1:
+                foreach (var makura in _makuraControllers)
+                {
+                    if (!makura.IsThrow)
+                    {
+                        makura.CurrentColorType = ColorChanger.ColorType.Green;
+                    }
+                }
+                break;
+            case 2:
+                foreach (var makura in _makuraControllers)
+                {
+                    if (!makura.IsThrow)
+                    {
+                        makura.CurrentColorType = ColorChanger.ColorType.Blue;
+                    }
+                }
+                break;
+            case 3:
+                foreach (var makura in _makuraControllers)
+                {
+                    if (!makura.IsThrow)
+                    {
+                        makura.CurrentColorType = ColorChanger.ColorType.Black;
+                    }
+                }
+                break;
+            case 4:
+                foreach (var makura in _makuraControllers)
+                {
+                    if (!makura.IsThrow)
+                    {
+                        makura.CurrentColorType = ColorChanger.ColorType.Nomal;
+                    }
+                }
+                break;
+        }
+    }
+    private Vector3 RandomPosition()
+    {
+        float xMin = -8.0f;
+        float xMax = 8.0f;
+        float zMin = -3.0f;
+        float zMax = 7.0f;
+        float y = 6.0f;
+
+        float randomX = Random.Range(xMin, xMax);
+        float randomZ = Random.Range(zMin, zMax);
+
+        return new Vector3(randomX, y, randomZ);
     }
 }
